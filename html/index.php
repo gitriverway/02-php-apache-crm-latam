1740298cbe5aa19d56a7735dc6299a8d4220a381
fausto <webriverway@gmail.com>
2026-02-06 19:17:14 -0500

    Update Language

--- html/index.php
+++ html/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/../../model/modelo_idioma.php';
+// require_once __DIR__ . '/../../model/modelo_idioma.php';
+require_once 'model/modelo_idioma.php';
 $t = function ($key) {
     return Modelo_Idioma::t($key);
 };
@@ -10,4 +11,4 @@ require_once "controller/controlador_plantilla.php";
 require_once "controller/clientes/controlador_cliente_contar_contrato_cliente.php";
 
 $plantilla = new Controlador_Plantilla();
-$plantilla -> ctrPlantilla();
+$plantilla->ctrPlantilla();
