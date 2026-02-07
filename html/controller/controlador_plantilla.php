f2ca2777bdf4ef641b46803f0d514f962620218d
fausto <webriverway@gmail.com>
2026-02-06 21:26:30 -0500

    Update Language

--- html/controller/controlador_plantilla.php
+++ html/controller/controlador_plantilla.php
@@ -1,16 +1,16 @@
 <?php
-require_once __DIR__ . '/../../model/modelo_idioma.php';
+require_once __DIR__ . '/../model/modelo_idioma.php';
 $t = function ($key) {
-    return Modelo_Idioma::t($key);
+	return Modelo_Idioma::t($key);
 };
 
 
-class Controlador_Plantilla{
+class Controlador_Plantilla
+{
 
-	static public function ctrPlantilla(){
+	static public function ctrPlantilla()
+	{
 
 		include "view/plantilla.php";
-
 	}
-
 }
